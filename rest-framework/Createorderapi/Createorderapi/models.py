15a580c8a7758d21528c357a6c0346a659d8ed15
Rehşan Yılmaz <akturkrehsan@gmail.com>
2022-05-26 17:12:02 +0300

    Update models.py

--- rest-framework/Createorderapi/Createorderapi/models.py
+++ rest-framework/Createorderapi/Createorderapi/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Shop_transaction(models.Model):
    
     id = models.IntegerField(db_column='id',primary_key=True)
-    user_id = models.CharField(db_column='user_id',max_length=100)
-    orderdate = models.CharField(db_column='transaction_type',max_length=100)
+    orderid = models.CharField(db_column='orderid',max_length=100)
+    orderdate = models.CharField(db_column='orderdate',max_length=100)
     user_email = models.CharField(db_column='user_email',max_length=100)
     pos_name = models.CharField(db_column='pos_name',max_length=100)
 
